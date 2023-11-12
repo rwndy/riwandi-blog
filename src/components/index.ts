70926e951c67bda39e6debc51120c27265563bf8
Riwandi <riwandi.zamani@mncgroup.com>
2023-11-13 01:14:47 +0700

    deleted component legacy

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
 import Meta from "./Meta";
 import HomeComponent from "./Home";
 import Modals from "./Global/Modals";
-import AboutMobile from "./AboutMobile";
 import Navigation from "./Navigation";
 import Layout from "./Layout";
 
-export { AboutMobile, HomeComponent, Meta, Modals, Navigation, Layout };
+export { HomeComponent, Meta, Modals, Navigation, Layout };
